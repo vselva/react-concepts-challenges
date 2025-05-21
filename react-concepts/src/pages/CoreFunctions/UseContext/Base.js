5a9db48c85188cea71b143adddf59859e6a1d1b9
Selvakumar <vselva@gmail.com>
2025-05-21 19:08:46 +0530

    Simple Context Exmaple

--- react-concepts/src/pages/CoreFunctions/UseContext/Base.js
+++ react-concepts/src/pages/CoreFunctions/UseContext/Base.js
@@ -1,6 +1,9 @@
+import { createContext, useContext } from "react";
 import ChildComponent from "./ChildComponent";
 import OnePageContextExample from "./OnePageContextExample";
 import ParentComponent from "./ParentComponent";
+
+const simpleContext = createContext();
 
 function UseContextBase() {
 
@@ -13,6 +16,24 @@ function UseContextBase() {
             </ParentComponent>
             <hr/>
             <OnePageContextExample /> 
+            <hr />
+
+            <simpleContext.Provider value={{ name: 'Selva' }}>
+                <UseContext />
+            </simpleContext.Provider>
+            
+        </>
+    );
+}
+
+function UseContext() {
+    const user = useContext(simpleContext);
+
+    return(
+        <>
+            <h4>UseContext (Child) Component:</h4>
+            <p>UseContext Component to receive value 'name' from Parent Component (UseContextBase):</p>
+            <p> Value passed: { user.name } </p>
         </>
     );
 }
