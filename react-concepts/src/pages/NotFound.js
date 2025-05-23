b9359c3ccadb0f13f8a52499afddfc66ba697a21
Selvakumar <vselva@gmail.com>
2025-05-23 08:12:08 +0530

    Added NotFound Component to show for invalid URL

---
+++ react-concepts/src/pages/NotFound.js
@@ -0,0 +1,12 @@
+
+function NotFound() {
+
+    return (
+        <>
+            <h1>404 Page Not Found!</h1>
+            <p>Kindly check the URL and try again!</p>
+        </>
+    );
+}
+
+export default NotFound;
