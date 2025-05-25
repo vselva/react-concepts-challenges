33d4db948c93876fb6f13df8c7bbaaadb8cca900
Selvakumar <vselva@gmail.com>
2025-05-25 18:16:14 +0530

    customHook for WindowSize Display

--- react-concepts/src/pages/Home.js
+++ react-concepts/src/pages/Home.js
@@ -1,35 +1,8 @@
-import { Link } from 'react-router-dom';
-
 function Home() {
 
     return (
         <div className="container mt-5">
             <h1 className="mb-4 text-center">Welcome to React Fundamentals!</h1>
-            <nav>
-                <ul className="navbar-nav flex-row justify-content-center">
-                    <li className="nav-item mx-2">
-                        <Link className="nav-link btn btn-outline-primary" to="/">Home</Link>
-                    </li>
-                    <li className="nav-item mx-2">
-                        <Link className="nav-link btn btn-outline-primary" to="/Jsx">Jsx</Link>
-                    </li>
-                    <li className="nav-item mx-2">
-                        <Link className="nav-link btn btn-outline-primary" to="/Props">Props</Link>
-                    </li>
-                    <li className="nav-item mx-2">
-                        <Link className="nav-link btn btn-outline-primary" to="/Map">Map</Link>
-                    </li>
-                    <li className="nav-item mx-2">
-                        <Link className="nav-link btn btn-outline-primary" to="/UseState">useState</Link>
-                    </li>
-                    <li className="nav-item mx-2">
-                        <Link className="nav-link btn btn-outline-primary" to="/Components">Components</Link>
-                    </li>
-                    <li className="nav-item mx-2">
-                        <Link className="nav-link btn btn-outline-primary" to="/UseEffect">UseEffect</Link>
-                    </li>
-                </ul>
-            </nav>
         </div>
     )
 }
--- react-concepts/src/pages/Hooks/CustomHook/Base.js
+++ react-concepts/src/pages/Hooks/CustomHook/Base.js
@@ -1,9 +1,13 @@
 import useCounter from "./useCounter";
+import UseWindowSize from "./useWindowSize";
 
 function CustomHook() {
 
-    // a
+    // custom hook useCounter
     const [ count, increment, decrement, reset ] = useCounter(0);
+
+    // custom hook - useWindowSize
+    const windowSize = UseWindowSize();
 
     return(
         <>
@@ -16,6 +20,10 @@ function CustomHook() {
                 <button type="button" className="btn btn-secondary" onClick={decrement}>Decrement</button>
                 <button type="button" className="btn btn-danger" onClick={reset}>Reset</button>
             </div>
+            <hr />
+            <h5 className="mb-2">useWindowSize():</h5>
+            <p className="mb-3">useWindowSize is a custom hook to return window size.</p>
+            <pre>windowSize: { windowSize.height } x { windowSize.width } </pre>
         </>
     );
 }
