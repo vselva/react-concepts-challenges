94272a57af082c8d9f5ee94bd072796e52ac1806
Selvakumar <vselva@gmail.com>
2025-07-12 12:34:04 +0530

    changes

--- react-concepts/src/pages/Hooks/6-useMemo.js/Base.js
+++ react-concepts/src/pages/Hooks/6-useMemo.js/Base.js
@@ -1,12 +1,21 @@
+import { useState } from "react";
 import DoubleValueMemoExample from "./CaculateFunctionWithUseMemo";
 
 const UseMemoBase = () => {
+    const [ toggle, setToggle ] = useState(true);
+
     return (
         <> 
             <h2>useMemo():</h2>
             <hr />
             
-            <DoubleValueMemoExample />
+            <DoubleValueMemoExample props={toggle} />
+
+             <button 
+                type="button" 
+                onClick={() => setToggle(prev => !prev)}>
+                    Toggle {toggle ? 'True' : 'False'}
+            </button>
             <hr />
         </>
     );
--- react-concepts/src/pages/Hooks/6-useMemo.js/CaculateFunctionWithUseMemo.js
+++ react-concepts/src/pages/Hooks/6-useMemo.js/CaculateFunctionWithUseMemo.js
@@ -1,9 +1,18 @@
-import { useMemo, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 
-function DoubleValueMemoExample() {
+function DoubleValueMemoExample(props) {
     const [count, setCount] = useState(0);
 
     // Memoized value that updates when count changes
+    // When the component rerenders, this call back function will executed even without count changes 
+    useEffect(() => {
+        console.log("Recomputing doubleCount...");
+        // setCount((count) => count * 2);
+        console.log(props);
+    }, []);
+
+    // Memoized value that updates when count changes
+    // Even the component rerenders this call back function will NOT executed untill count changes 
     const doubleCount = useMemo(() => {
         console.log("Recomputing doubleCount...");
         return count * 2;
@@ -20,10 +29,12 @@ function DoubleValueMemoExample() {
             <h4 className="mb-3">useMemo Demonstration</h4>
 
             <p className="alert alert-primary">
-                <strong>Double Count (with Dependency):</strong> {doubleCount}
+                <strong>Double Count (with Dependency):</strong> 
+                {doubleCount}
             </p>
             <p className="alert alert-secondary">
-                <strong>Initial Double Count (no Dependency):</strong> {initialDoubleCount}
+                <strong>Initial Double Count (no Dependency):</strong>
+                {initialDoubleCount}
             </p>
 
             <button
@@ -33,6 +44,7 @@ function DoubleValueMemoExample() {
             >
                 Increment Count ({count})
             </button>
+            <p>Toggle via Props: {props.toggle ? 'TRUE' : 'FALSE'}</p>
         </div>
     );
 }
