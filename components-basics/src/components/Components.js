bcf6032ab53331c0fe5885fe6f2c581bc9a64b6a
Selvakumar <vselva@gmail.com>
2025-08-05 21:40:12 +0530

    Removed

--- components-basics/src/components/Components.js
+++
@@ -1,9 +0,0 @@
-import ClassComponent from "./ClassComponent"
-import FunctionalComponent from "./FunctionalComponent"
-
-export default function Components () {
-    return <>
-        <FunctionalComponent />
-        <ClassComponent />
-    </>
-}
