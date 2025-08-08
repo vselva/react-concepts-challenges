34a9e2a7d9ed9e2830dedc754ec9a0acb4093970
Selvakumar <vselva@gmail.com>
2025-08-08 13:08:06 +0530

    ESLint Installation & Initialization
    npm install eslint -g ==> Installation
    npx eslint --init        ==> initialization

--- react-concepts/src/pages/Components/HigherOrderComponent/Base.js
+++ react-concepts/src/pages/Components/HigherOrderComponent/Base.js
@@ -4,16 +4,23 @@ import HigherOrderComponent from "./HigherOrderComponent";
 const EnhancedComponent = HigherOrderComponent(WrappedComponent);
 
 function HigherOrderComponentBase() {
-    return(
+    return (
         <>
             <h4>4. Higher Order Component</h4>
-            <p><strong>This is the react component which takes component as argument and returns a new component. This is an advanced pattern.</strong></p>
+            <p>
+                <strong>
+                    This is the react component which takes component as
+                    argument and returns a new component. This is an advanced
+                    pattern.
+                </strong>
+            </p>
 
-            <WrappedComponent data="Original component (no HOC applied)" />
-
-            <EnhancedComponent  isLoading={ true } />
+            <EnhancedComponent isLoading={true} />
             <br />
-            <EnhancedComponent  isLoading={ false } data="Enhanced by HOC with isLoading is False" />
+            <EnhancedComponent
+                isLoading={false}
+                data="Enhanced by HOC with isLoading is False"
+            />
         </>
     );
 }
